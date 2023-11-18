dd664b008bd944e294a973ccbff06e9a09b8cfbf
Muhammad Luqman Asghar <luqmanposwal786@gmail.com>
2023-11-18 22:33:02 +0500

    next.js

---
+++ app/page.tsx
@@ -0,0 +1,59 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main>
+      <title>First Project</title> 
+      <h1>Certified Web 3.0 and Metaverse Developer: A Nationwide Program in Karachi, Lahore, Islamabad, and Peshawar</h1>
+      <p>Getting Ready for the Next Generation and Future of the Internet - Join a 13 Trillion Dollar Industry with 5 Billion Users</p>
+      <h2>Certified Web 3.0 and Metaverse Developer: A Nationwide Program in Karachi, Lahore, Islamabad, and Peshawar</h2> 
+       <img src="https://www.panaverse.co/red-p-logo-text_dao_croped.png" alt="panaverse image" /> 
+              <h3>The Future of the Web is Web 3.0, Metaverse, and Edge Computing. Panaverse DAO is a movement to spread these technolgies globally. It is community of Web 3 and Metaverse developers, designers, trainers, startup founders and service providers.</h3>
+       <video src="C:\Users\HP\next.js\public"></video>
+        <h4>
+Admissions Now Open in Karachi, Lahore, Islamabad, and Peshawar
+This document on Google Docs
+</h4> 
+<p>The internet is without a doubt the most important technological development in human history. Web3 and metaverse technologies expand the internet as we know it by introducing novel features and advancements. Metaverse will make use of all aspects of modern technology, including 3D, VR, AR, AI, blockchain, cloud computing, voice computing, ambient computing, and more.
+
+Citi is the latest Wall Street business to give a positive prognosis for Web 3.0 and the Metaverse, terms used to depict a future internet vision centred on decentralised technologies and virtual worlds. Citi stated in a March 2022 research paper that the metaverse economy might have a total addressable market of up to $13 trillion and five billion people by 2030.</p>
+<h5>The Program in a Nutshell: Earn While You Learn</h5>
+<p>In this brand-new type of curriculum, students will learn how to make money and boost exports in the classroom and will begin doing so within six months of the program’s beginning. It resembles a cross between a corporate venture and an educational project.</p>
+<h5>Program of Studies</h5> 
+<p>This curriculum is intended for beginners who want to learn software development from the ground up. The first two quarters are shared by all specialities and are dedicated to studying Object-Oriented Programming and cutting-edge Full-Stack Web 2.0 development. It is going to be a year-long hybrid programme that includes both onsite and online classes and is divided into four quarters of 13 weeks each. The emphasis will be on hands-on learning by educating students to produce projects. To accommodate everyone, courses will be held primarily on weekends or after 6:00 p.m. (Pakistan Time) on weekdays. It employs a hybrid teaching format, with core onsite classes complemented by online Zoom laboratories and recorded videos.</p>
+<h5>Core Courses (Common in All Specializations)</h5> 
+<p>Every participant of the program will start by completing the following two core courses:</p>
+<h6>
+<iframe width="560" height="315" src="https://youtu.be/gGVqxEu2PaA?si=tvqDfAZk2vHupqWG" FrameBorder="0" allowfullscreen></iframe>
+Quarter I (Core) CS-101: Object-Oriented Programming using TypeScript
+
+Quarter II (Core) W2-201: Developing Planet-Scale Web 2.0 Serverless Cloud Cloud Apps and APIs using Next.js 13 and Cloud Development Kit (CDK) for Terraform</h6>
+<section>
+            <table>
+                <tr>
+                    <th>Header 1</th>
+                    <th>Header 2</th>
+                </tr>
+                <tr>
+                    <td>Data 1</td>
+                    <td>Data 2</td>
+                </tr>
+            </table>
+        </section>
+<section>
+            <h2>Contact Us</h2>
+            
+                <form action="submit">
+                <label>Name:</label><br/>
+                <input type="text" id="name" name="name"><br/>
+
+                <label >Email:</label><br/>
+                <input type="email" id="email" name="email"><br/>
+
+                <input type="submit" value="Submit">
+            </form>
+        </section>
+
+    </main>
+  )
+}
